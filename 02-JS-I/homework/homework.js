4e533cb9db723788188e3916d9fa138609322781
germanmoreno17 <gams1761@gmail.com>
2022-04-20 17:16:08 -0400

    otra modi

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,10 +4,10 @@
 const nuevaString = "hola";
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = "5";
+const nuevoNum = 5 ; 
 
 // Crea una variable booleana:
-const nuevoBool = "true";
+const nuevoBool = true ;
 
 // Resuelve el siguiente problema matemático:
 const nuevaResta = 10 - 5 === 5;
